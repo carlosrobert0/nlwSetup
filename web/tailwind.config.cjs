bb164e771f0a8f6dcfd5c2322aba0442a3b43daf
carlosrobert0 <carlosroberto15755@gmail.com>
2023-01-18 16:17:23 -0300

    Estrutura de homepage, header, tabela habitos diarios gração de range de dias no fim

--- web/tailwind.config.cjs
+++ web/tailwind.config.cjs
@@ -5,7 +5,22 @@ module.exports = {
     './index.html'
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        background: '#09090a'
+      },
+
+      fontFamily: {
+        regular: 'Inter_400Regular',
+        semibold: 'Inter_600SemiBold',
+        bold: 'Inter_700Bold',
+        extrabold: 'Inter_800ExtraBold'
+      },
+      
+      gridTemplateRows: {
+        7: 'repeat(7, minmax(0, 1fr))'
+      },
+    },
   },
   plugins: [],
 }
