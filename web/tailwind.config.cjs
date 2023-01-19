0a8cd2a5fadcb5df9d43544f96dac30ae2a7dfa5
carlosrobert0 <carlosroberto15755@gmail.com>
2023-01-18 22:44:24 -0300

    atualizando tailwind.config.cjs na web

--- web/tailwind.config.cjs
+++ web/tailwind.config.cjs
@@ -9,13 +9,6 @@ module.exports = {
       colors: {
         background: '#09090a'
       },
-
-      fontFamily: {
-        regular: 'Inter_400Regular',
-        semibold: 'Inter_600SemiBold',
-        bold: 'Inter_700Bold',
-        extrabold: 'Inter_800ExtraBold'
-      },
       
       gridTemplateRows: {
         7: 'repeat(7, minmax(0, 1fr))'
