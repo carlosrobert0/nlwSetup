b91ec50599444d489f0a7b413185dee114df4994
carlosrobert0 <carlosroberto15755@gmail.com>
2023-01-20 18:59:51 -0300

    Criação de telas de criar habito e detalhe do dia, implementação de navegacao

--- mobile/src/screens/Home.tsx
+++ mobile/src/screens/Home.tsx
@@ -4,6 +4,7 @@ import { generateRangeDatesFromYearStart } from "../utils/generate-range-between
 
 import { HabitDay, DAY_SIZE } from "../components/HabitDay";
 import { Header } from "../components/Header";
+import { useNavigation } from "@react-navigation/native";
 
 const weekDays = ['D', 'S', 'T', 'Q', 'Q', 'S', 'S']
 const datesFromYearStart = generateRangeDatesFromYearStart()
@@ -11,6 +12,8 @@ const minimumSummaryDatesSizes = 18 * 5
 const amountOfDaysToFill = minimumSummaryDatesSizes - datesFromYearStart.length
 
 export function Home() {
+  const { navigate } = useNavigation()
+  
   return (
     <View className="flex-1 bg-background px-8 pt-16">
       <Header />
@@ -40,6 +43,7 @@ export function Home() {
             datesFromYearStart.map((date: any) => (
               <HabitDay
                 key={date.toISOString()}
+                onPress={() => navigate('habit', { date: date.toISOString() })}
               />
             ))
           }
