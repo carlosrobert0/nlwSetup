a030d87c4cd4af2e0380a4eea2bfca6ae821b091
carlosrobert0 <carlosroberto15755@gmail.com>
2023-01-21 00:54:37 -0300

    dados dinamicos de habitos totais e completos

--- web/src/components/HabitDay.tsx
+++ web/src/components/HabitDay.tsx
@@ -20,8 +20,8 @@ export function HabitDay({ amount = 0, completed = 0, date }: HabitDayProps) {
     <Popover.Root>
       <Popover.Trigger
         className={clsx('w-10 h-10 border-2 rounded-lg', {
-          'bg-zinc-900 border-zinc-800': completedPercentage === 20,
-          'bg-violet-900 border-violet-700': completedPercentage >= 0 && completedPercentage < 20,
+          'bg-zinc-900 border-zinc-800': completedPercentage === 0,
+          'bg-violet-900 border-violet-700': completedPercentage > 0 && completedPercentage < 20,
           'bg-violet-800 border-violet-600': completedPercentage >= 20 && completedPercentage < 40,
           'bg-violet-700 border-violet-500': completedPercentage >= 40 && completedPercentage < 60,
           'bg-violet-600 border-violet-500': completedPercentage >= 60 && completedPercentage < 80,
