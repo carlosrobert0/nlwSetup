82a4601c8ababc6d76f4c4a01060ac5d8b50e8f7
carlosrobert0 <carlosroberto15755@gmail.com>
2023-01-25 03:13:19 -0300

    Centralizando icones em butao de login social

--- web/src/components/SignInSocialButton.tsx
+++ web/src/components/SignInSocialButton.tsx
@@ -9,14 +9,14 @@ interface Props extends ButtonHTMLAttributes<HTMLElement> {
 export function SignInSocialButton({ title, svg, ...rest }: Props) {
   return (
     <button
-      className="mt-4 mb-4 flex h-14 w-52 items-center justify-center rounded-md border-b border-green-400 hover:bg-gradient-to-t from-green-900 via-transparent to-transparent transition-all"
+      className="flex h-14 w-50 items-center justify-start rounded-md border-b border-green-400 hover:bg-gradient-to-t from-green-900 via-transparent to-transparent transition-all"
       {...rest}
     >
-      <div className="flex h-full items-center justify-center p-4">
+      <div className="flex h-full items-center justify-center pl-2 pr-4">
         <ReactSVG src={svg} />
       </div>
 
-      <h2 className="text-center font-heebo text-base">{title}</h2>
+      <h2 className="text-center font-heebo text-base pr-2">{title}</h2>
     </button>
   )
 }
